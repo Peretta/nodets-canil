f5cb210bfc5bea2fcb939ff8e97ac45af00aae7a
vex <vexstartup@gmail.com>
2022-05-11 14:48:52 -0300

    Modificando os dados em cada página

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,17 +1,38 @@
 import { Request, Response } from 'express';
 
 export const home = (req: Request, res: Response) => {
-   res.render('pages/page');
+   res.render('pages/page', {
+    banner: {
+           title: 'Todos os animais',
+           background: 'allanimals.jpg' 
+    }
+   });
+       
 }
 export const dogs = (req: Request, res: Response) => {
-   res.send('dogs')
-    // res.render('pages/page');
+
+    res.render('pages/page', {
+        banner: {
+               title: 'Todos os cachorros',
+               background: 'banner_dog.jpg' 
+        }
+       });
 }
 export const cats = (req: Request, res: Response) => {
-    res.send('cats')
-    // res.render('pages/page');
+
+    res.render('pages/page', {
+        banner: {
+               title: 'Todos os gatos',
+               background: 'banner_cat.jpg' 
+        }
+       });
 }
 export const fishes = (req: Request, res: Response) => {
-    res.send('fishes')
-    // res.render('pages/page');
+
+    res.render('pages/page', {
+        banner: {
+               title: 'Todos os peixes',
+               background: 'banner_fish.jpg' 
+        }
+       });
 }
